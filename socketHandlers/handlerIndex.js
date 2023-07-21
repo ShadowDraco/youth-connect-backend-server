1c32bb50d7b1d613f2a39c3d6dfb4265f56665dd
Ethan Storm <stormethan10@gmail.com>
2023-07-21 14:31:36 -0700

    fix uuid import

--- socketHandlers/handlerIndex.js
+++ socketHandlers/handlerIndex.js
@@ -2,7 +2,7 @@
 
 const axios = require("axios");
 
-import { v4 as uuidv4 } from "uuid";
+const { v4: uuidv4 } = require("uuid");
 const Filter = require("bad-words");
 const filter1 = new Filter();
 const filter2 = require("leo-profanity");
